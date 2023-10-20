6bb26acc223a6d8355d9707c2d8924cc0c9bc53f
Tropicalbunny <charlie.penfold@hotmail.co.uk>
2023-10-20 11:07:52 +0100

    info tweaks

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,8 +4,12 @@ console.log("hello world")
 let infoOpen = document.getElementById('info-open');
 let infoContainer = document.getElementById('info-container');
 let infoClose = document.getElementById('info-close');
+let infoOpen2 = document.getElementById('info-open2');
 
 infoOpen.addEventListener('click', function(){
+    infoContainer.style.display = 'block';
+});
+infoOpen2.addEventListener('click', function(){
     infoContainer.style.display = 'block';
 });
 
