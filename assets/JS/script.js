c1936017f1463fe1c0c1f3a2285ae424a054a075
Tropicalbunny <charlie.penfold@hotmail.co.uk>
2023-10-20 11:04:54 +0100

    popup created and working

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,14 @@
 console.log("hello world")
+// info popup 
+
+let infoOpen = document.getElementById('info-open');
+let infoContainer = document.getElementById('info-container');
+let infoClose = document.getElementById('info-close');
+
+infoOpen.addEventListener('click', function(){
+    infoContainer.style.display = 'block';
+});
+
+infoClose.addEventListener('click', function(){
+    infoContainer.style.display = 'none';
+})
