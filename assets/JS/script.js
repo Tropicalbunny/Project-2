d8db044622ab6490f4daf85bd8e53ba558a19de6
Tropicalbunny <charlie.penfold@hotmail.co.uk>
2023-10-20 12:32:49 +0100

    created transistions between pages

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,18 +1,28 @@
-console.log("hello world")
-// info popup 
+// info popup functions
 
 let infoOpen = document.getElementById('info-open');
 let infoContainer = document.getElementById('info-container');
 let infoClose = document.getElementById('info-close');
-let infoOpen2 = document.getElementById('info-open2');
+
+console.log(infoOpen)
 
 infoOpen.addEventListener('click', function(){
     infoContainer.style.display = 'block';
-});
-infoOpen2.addEventListener('click', function(){
-    infoContainer.style.display = 'block';
-});
+})
 
 infoClose.addEventListener('click', function(){
     infoContainer.style.display = 'none';
+})
+
+//page transition
+
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
 })
